4a1f0a880d153c121333b30b62278cb00103c290
Леонид Иванов <alexeysoloberezinsolo@gmail.com>
2024-10-27 05:46:15 +0800

    S

--- src/components/core/WidgetBooking.js
+++ src/components/core/WidgetBooking.js
@@ -1,91 +1,77 @@
-'use client';
+"use client";
 
-import React, {useEffect} from 'react';
+import Script from 'next/script';
+import Head from 'next/head';
+import React from 'react';
 
-const SkipperWidget = ({onInitSkipper}) => {
-  let script1 = null;
-  let link1 = null;
-  let link2 = null;
-  let link3 = null;
-
-  useEffect(() => {
-    setTimeout(() => {
-      script1 = document.createElement("script");
-      script1.src = "https://widget.skipperhospitality.com/cash-sdk.umd.js";
-      script1.async = true;
-      script1.type = "module";
-      document.head.appendChild(script1);
-  
-      link1 = document.createElement("link");
-      link1.rel = "preload";
-      link1.href = "https://widget.skipperhospitality.com/style.css";
-      link1.as = "style";
-      link1.crossOrigin = "anonymous";
-      document.head.appendChild(link1);
-  
-      link2 = document.createElement("link");
-      link2.rel = "preload";
-      link2.href = "https://widget.skipperhospitality.com/site-base.css";
-      link2.as = "style";
-      link2.crossOrigin = "anonymous";
-      document.head.appendChild(link2);
-  
-      link3 = document.createElement("link");
-      link3.rel = "preconnect";
-      link3.href = "https://cash-api.skipperhospitality.com";
-      document.head.appendChild(link3);
-  
-      script1.onload = () => {
-        if (window.initSkipper) {
-          window.initSkipper({
-            hotelId: "IRIS_HOTEL_BROOKLINE",
-            target: "skipper-target",
-            isDynamic: true,
-            targetIds: ["placeholder-id"],
-            targetClasses: ["placeholder-class"],
-            fallbackUrl: "",
-            style: {
-              primaryFont: {
-                family: "questrial, sans-serif",
-                transform: "uppercase"
-              },
-              secondaryFont: "questrial, sans-serif",
-              primaryColor: "#644FF5",
-              accentColor: "#CCBFB3",
-              primaryFontColor: "#211A52",
-              accentFontColor: "#fff",
-              placeholderFontColor: "#fff",
-              secondaryFontColor: "#5C4A53",
-              primaryBackgroundColor: "#fff",
-              primaryBorderColor: "#433E6B",
-              secondaryBackgroundColor: "#fff",
-              inactiveOpacity: 0.5,
-              disabledOpacity: 0.3,
-              highlightOpacity: 0.1,
-              stateOpacity: 0.4
-            }
-          });
-
-          onInitSkipper()
+const SkipperWidget = ({ onInitSkipper }) => {
+  // Функция инициализации, вызывается после загрузки скрипта
+  const handleSkipperLoad = () => {
+    if (window.initSkipper) {
+      window.initSkipper({
+        hotelId: "IRIS_HOTEL_BROOKLINE",
+        target: "skipper-target",
+        isDynamic: true,
+        targetIds: ["placeholder-id"],
+        targetClasses: ["placeholder-class"],
+        fallbackUrl: "",
+        style: {
+          primaryFont: {
+            family: "questrial, sans-serif",
+            transform: "uppercase"
+          },
+          secondaryFont: "questrial, sans-serif",
+          primaryColor: "#644FF5",
+          accentColor: "#CCBFB3",
+          primaryFontColor: "#211A52",
+          accentFontColor: "#fff",
+          placeholderFontColor: "#fff",
+          secondaryFontColor: "#5C4A53",
+          primaryBackgroundColor: "#fff",
+          primaryBorderColor: "#433E6B",
+          secondaryBackgroundColor: "#fff",
+          inactiveOpacity: 0.5,
+          disabledOpacity: 0.3,
+          highlightOpacity: 0.1,
+          stateOpacity: 0.4
         }
-      };
-    }, 0)
+      });
 
-    return () => {
-      script1 && document.head.removeChild(script1);
-      link1 && document.head.removeChild(link1);
-      link2 && document.head.removeChild(link2);
-      link3 && document.head.removeChild(link3);
-    };
-  }, []);
+      onInitSkipper && onInitSkipper();
+    }
+  };
 
+  return (
+    <>
+      <Head>
+        <link
+          rel="preload"
+          href="https://widget.skipperhospitality.com/style.css"
+          as="style"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="https://widget.skipperhospitality.com/site-base.css"
+          as="style"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preconnect"
+          href="https://cash-api.skipperhospitality.com"
+        />
+      </Head>
 
-    return (
-      <div>
-        <div id="skipper-target"></div>
-      </div>
-    );
-  }
-;
+      <Script
+        src="https://widget.skipperhospitality.com/cash-sdk.umd.js"
+        type="module"
+        strategy="afterInteractive" // Загружает скрипт после первичного рендера
+        onLoad={handleSkipperLoad} // Инициализируем виджет после загрузки скрипта
+      />
+
+      <div id="skipper-target"></div>
+    </>
+  );
+};
 
 export default SkipperWidget;
