5b398092cba82cbca2d8689cefc97351a55b8ce7
Леонид Иванов <alexeysoloberezinsolo@gmail.com>
2024-10-27 12:51:19 +0800

    asd

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,29 +2,23 @@ import "./globals.css";
 import "./styles/index.scss"
 import 'swiper/css';
 import Footer from "@/components/core/Footer";
-import BookingProvider from "@/context/BookingProvider/BookingProvider";
-import Loader from "@/components/core/Loader";
 import LoadChatScript from "@/components/core/LoadChatScript";
-import Script from "next/script";
-import Head from "next/head";
 import Aos from "@/components/Aos";
 import WidgetBooking from "@/components/core/WidgetBooking";
 
 export default function RootLayout({children}) {
 
   return (
-    <html lang="en">
+    <html lang="ru">
     <body
       className={`antialiased`}
     >
-    <BookingProvider>
-      {children}
-      <Footer/>
-      {/*<Loader/>*/}
-      <Aos />
-      <LoadChatScript />
-      <WidgetBooking />
-    </BookingProvider>
+    {children}
+    <Footer/>
+    {/*<Loader/>*/}
+    <Aos/>
+    <LoadChatScript/>
+    <WidgetBooking/>
     </body>
     </html>
   );
