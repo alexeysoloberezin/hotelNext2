9b1fe99145eca6cbd25e47efe753b262231a5cac
Леонид Иванов <alexeysoloberezinsolo@gmail.com>
2024-12-08 07:06:10 +0800

    hotel zcx

--- src/components/core/Header.js
+++ src/components/core/Header.js
@@ -57,6 +57,14 @@ const Header = ({transparent}) => {
       img: "/navImg1",
       name: "About Us",
     },
+    {
+      dataTestid: '',
+      target: '',
+      className: '',
+      to: "/events",
+      img: "/navImg6",
+      name: "Events"
+    },
   ];
 
   const handleClickLink = (e, to) => {
