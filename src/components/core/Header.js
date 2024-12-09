80027398785d5eab97251ede1d2bd442ba6910a5
Leonid Ivanov <71139559+alexeysoloberezin@users.noreply.github.com>
2024-12-10 02:22:28 +0800

    Update Header.js

--- src/components/core/Header.js
+++ src/components/core/Header.js
@@ -62,15 +62,7 @@ const Header = ({transparent}) => {
       img: "/navImg1",
       name: "About Us",
     },
-    {
-      id: 6,
-      dataTestid: '',
-      target: '',
-      className: '',
-      to: "/events",
-      img: "/navImg6",
-      name: "Events"
-    },
+    
   ];
 
   const handleClickLink = (e, to) => {
