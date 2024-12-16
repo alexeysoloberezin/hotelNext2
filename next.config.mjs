9d21ad8252261e82087eb66b7f973fbd8ccfa79f
Леонид Иванов <alexeysoloberezinsolo@gmail.com>
2024-12-17 03:50:26 +0800

    s

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.stats = "errors-only"; // Показывать только ошибки
+    return config;
+  },
+};
 
 export default nextConfig;
